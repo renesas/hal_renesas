9fd90cdc3180e33954de3fccbc2237537dfad523
Duy Phuong Hoang. Nguyen <duy.nguyen.xa@renesas.com>
2024-06-18 15:40:45 +0700

    hal: renesas: ra: Update bsp config file for RA8D1 and RA8T1
    This update is to adapt with new folder architecture of hal renesas
    for RA8D1 and RA8T1
    
    Signed-off-by: Duy Phuong Hoang. Nguyen <duy.nguyen.xa@renesas.com>

---
+++ zephyr/ra/ra_cfg/fsp_cfg/bsp/ra8d1/bsp_mcu_device_pn_cfg.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024 Renesas Electronics Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef BSP_MCU_DEVICE_PN_CFG_H_
+
+#define BSP_MCU_R7FA8D1BHECBD
+#define BSP_PACKAGE_PINS (224)
+
+#endif /* BSP_MCU_DEVICE_PN_CFG_H_ */
