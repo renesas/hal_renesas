bddceca81c60441809b113d9b969b8cdfcb041c8
Duy Phuong Hoang. Nguyen <duy.nguyen.xa@renesas.com>
2024-06-18 18:40:32 +0700

    hal: renesas: ra: Update require information for flash build
    Update version and license for RA8D1 and RA8T1 header files
    Update Data flash size and ROM size informnation for RA8D1
    RA8T1 and RA8M1
    
    Signed-off-by: Duy Phuong Hoang. Nguyen <duy.nguyen.xa@renesas.com>

--- zephyr/ra/ra_cfg/fsp_cfg/bsp/ra8t1/bsp_mcu_device_pn_cfg.h
+++ zephyr/ra/ra_cfg/fsp_cfg/bsp/ra8t1/bsp_mcu_device_pn_cfg.h
@@ -1,9 +1,11 @@
 /*
- * Copyright (c) 2024 Renesas Electronics Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+* Copyright (c) 2020 - 2024 Renesas Electronics Corporation and/or its affiliates
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
 #ifndef BSP_MCU_DEVICE_PN_CFG_H_
 #define BSP_MCU_DEVICE_PN_CFG_H_
+#define BSP_ROM_SIZE_BYTES (2064384)
+#define BSP_DATA_FLASH_SIZE_BYTES (12288)
 #define BSP_PACKAGE_PINS (224)
 #endif /* BSP_MCU_DEVICE_PN_CFG_H_ */
