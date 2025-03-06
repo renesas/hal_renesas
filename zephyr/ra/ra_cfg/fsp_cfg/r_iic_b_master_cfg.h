9f5ea8b33c5aae1c429916c3bcf16b3442b00dda
The Nguyen <the.nguyen.yf@renesas.com>
2025-03-06 16:17:23 +0700

    hal: renesas: ra: add support for r_iic_b_master
    First commit to initial support for r_iic_b_master
    
    Signed-off-by: The Nguyen <the.nguyen.yf@renesas.com>

---
+++ zephyr/ra/ra_cfg/fsp_cfg/r_iic_b_master_cfg.h
@@ -0,0 +1,20 @@
+/*
+* Copyright (c) 2020 - 2025 Renesas Electronics Corporation and/or its affiliates
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*/
+
+#ifndef R_IIC_B_MASTER_CFG_H_
+#define R_IIC_B_MASTER_CFG_H_
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define IIC_B_MASTER_CFG_PARAM_CHECKING_ENABLE (BSP_CFG_PARAM_CHECKING_ENABLE)
+#define IIC_B_MASTER_CFG_DTC_ENABLE (0)
+#define IIC_B_MASTER_CFG_ADDR_MODE_10_BIT_ENABLE (0)
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* R_IIC_B_MASTER_CFG_H_ */
